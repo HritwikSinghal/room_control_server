9b9cba0032108d6c7ea8ed5d3b28c39d26c256bf
Hritwik <Hritwiksinghal@outlook.in>
2021-07-12 22:25:00 +0530

    IRC: add parser, refine

--- static/ir_control.py
+++ static/ir_control.py
@@ -1,3 +1,4 @@
+import argparse
 import signal
 import sys
 import threading
@@ -50,90 +51,102 @@ def ir_out_control():
             threading.Event().wait(0.5)
 
 
-if __name__ == "__main__":
-    try:
+def control(person=0):
+    print(f"Initial Persons in room = {person}")
 
-        total_person = 0
-        room_in_ir_time = -1
-        room_out_ir_time = -1
+    total_person = person
+    room_in_ir_time = -1
+    room_out_ir_time = -1
 
-        in_ir_updated = 0
-        out_ir_updated = 0
+    in_ir_updated = 0
+    out_ir_updated = 0
 
-        # creating thread
-        t1 = threading.Thread(target=ir_in_control)
-        t2 = threading.Thread(target=ir_out_control)
+    # creating thread
+    t1 = threading.Thread(target=ir_in_control)
+    t2 = threading.Thread(target=ir_out_control)
 
-        # starting thread 1
-        t1.start()
-        # starting thread 2
-        t2.start()
+    # starting thread 1
+    t1.start()
+    # starting thread 2
+    t2.start()
 
-        while True:
-            try:
-                if in_ir_updated == 1 and out_ir_updated == 0:
-                    print("# person moving out and is between in and out sensor")
-                    # person moving out and is between in and out sensor
-                    threading.Event().wait(1)
+    while True:
+        try:
+            if in_ir_updated == 1 and out_ir_updated == 0:
+                print("# person moving out and is between in and out sensor")
+                # person moving out and is between in and out sensor
+                threading.Event().wait(1)
 
-                    if out_ir_updated == 0:
-                        # false alarm
-                        in_ir_updated = 0
-                        out_ir_updated = 0
-                        print("false alarm")
-                        continue
+                if out_ir_updated == 0:
+                    # false alarm
+                    in_ir_updated = 0
+                    out_ir_updated = 0
+                    print("false alarm")
+                    continue
 
-                    elif room_out_ir_time - room_in_ir_time < 2:
-                        # person moved out
-                        in_ir_updated = 0
-                        out_ir_updated = 0
+                elif room_out_ir_time - room_in_ir_time < 2:
+                    # person moved out
+                    in_ir_updated = 0
+                    out_ir_updated = 0
 
-                        room_in_ir_time = -1
-                        room_out_ir_time = -1
+                    room_in_ir_time = -1
+                    room_out_ir_time = -1
 
-                        if total_person == 1:
-                            room_control.person_zero()
-                        total_person = max(0, total_person - 1)
-                        print("Someone went out of room, Relax!")
-                        print("Total person", total_person, end='\n\n')
+                    if total_person == 1:
+                        room_control.person_zero()
+                    total_person = max(0, total_person - 1)
+                    print("Someone went out of room, Relax!")
+                    print("Total person", total_person, end='\n\n')
 
-                    else:
-                        print("some error in Moving out code, error code : 1")
+                else:
+                    print("some error in Moving out code, error code : 1")
 
-                elif out_ir_updated == 1 and in_ir_updated == 0:
-                    print("# person coming in and in between out and in sensor")
-                    # person coming in and in between out and in sensor
-                    threading.Event().wait(1)
+            elif out_ir_updated == 1 and in_ir_updated == 0:
+                print("# person coming in and in between out and in sensor")
+                # person coming in and in between out and in sensor
+                threading.Event().wait(1)
 
-                    if in_ir_updated == 0:
-                        # false alarm
-                        in_ir_updated = 0
-                        out_ir_updated = 0
-                        print("false alarm")
-                        continue
+                if in_ir_updated == 0:
+                    # false alarm
+                    in_ir_updated = 0
+                    out_ir_updated = 0
+                    print("false alarm")
+                    continue
 
-                    elif room_in_ir_time - room_out_ir_time < 2:
-                        # person came in
-                        in_ir_updated = 0
-                        out_ir_updated = 0
+                elif room_in_ir_time - room_out_ir_time < 2:
+                    # person came in
+                    in_ir_updated = 0
+                    out_ir_updated = 0
 
-                        room_in_ir_time = -1
-                        room_out_ir_time = -1
-                        if total_person == 0:
-                            room_control.person_one()
-                        total_person = min(15, total_person + 1)
-                        print("Someone came in room, Beware!")
-                        print("Total person", total_person, end='\n\n')
-            except:
-                traceback.print_exc()
-                in_ir_updated = 0
+                    room_in_ir_time = -1
+                    room_out_ir_time = -1
+                    if total_person == 0:
+                        room_control.person_one()
+                    total_person = min(15, total_person + 1)
+                    print("Someone came in room, Beware!")
+                    print("Total person", total_person, end='\n\n')
+            else:
                 out_ir_updated = 0
-                print("New Try except block")
-                signal.signal(signal.SIGINT, exit(0))
-                continue
+                in_ir_updated = 0
 
-    except:
-        print("There was some error in threading! \n")
-        traceback.print_exc()
-        sys.exit(0)
-        # GPIO.cleanup()
+        except:
+            traceback.print_exc()
+            in_ir_updated = 0
+            out_ir_updated = 0
+            print("New Try except block")
+            continue
+
+
+if __name__ == "__main__":
+    print("Starting Module")
+    
+    parser = argparse.ArgumentParser(description='IR Control')
+    parser.add_argument('-p', '--person', type=int, help="Number of persons in room", required=False, nargs=1)
+
+    # Parse and print the results
+    args = parser.parse_args()
+
+    if args.person:
+        control(args.person[0])
+    else:
+        control()
